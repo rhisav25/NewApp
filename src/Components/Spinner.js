c3081d8769b66449f0a40f24c9f4a548fe553d7e
rhisav25 <88587556+rhisav25@users.noreply.github.com>
2023-01-20 01:10:56 +0530

    Add files via upload

---
+++ src/Components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import loading from '../Loading_icon.gif'
+
+export class spinner extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img className="my-3" src={loading} alt="loading" />
+      </div>
+    )
+  }
+}
+
+export default spinner
